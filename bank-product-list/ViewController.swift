097892775ab5af6f18a9b63513bd9e3909674881
Filipe Marques <filipenmarques1@gmail.com>
2024-08-20 11:04:28 -0300

    Initial Commit

---
+++ bank-product-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bank-product-list
+//
+//  Created by Filipe Marques on 20/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bank-product-listUITests/bank_product_listUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bank_product_listUITestsLaunchTests.swift
+//  bank-product-listUITests
+//
+//  Created by Filipe Marques on 20/08/24.
+//
+
+import XCTest
+
+final class bank_product_listUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
